98ab8614cd8bcc8e722c3c843de1e2b850f5ec5a
Jenny Pavlova <jennypavlova94@gmail.com>
2018-02-26 15:55:22 +0200

    Added more animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 const Aframe = require('aframe');
+const $ = (query) => document.querySelector(query);
+
+const saturn = document.querySelectorAll(".saturn");
+
+saturn[0].addEventListener("click", function(){
+	saturn[1].click();
+});
+saturn[1].addEventListener("click", function(){
+	saturn[0].click();
+});
